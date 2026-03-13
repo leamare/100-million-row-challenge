5f6a4aeba8c3b832522bc242998b4e4521904dac
Lea Silakov <me@ileamare.ru>
2026-03-13 03:35:35 +0300

    cleaned up the file read logic and global namespace for funcs (31.45s)

--- app/Parser.php
+++ app/Parser.php
@@ -2,19 +2,18 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
-    private const int READ_CHUNK = 163_840;
-    private const int WRITE_BUF = 1_048_576;
+    private const READ_CHUNK = 163_840;
+    private const PREFIX_LEN = 25; // "https://stitcher.io/blog/"
+    private const WRITE_BUF  = 1_048_576;
 
     public function parse(string $inputPath, string $outputPath): void
     {
-        gc_disable();
+        \gc_disable();
 
-        $fileSize = filesize($inputPath);
-        
+        $fileSize = \filesize($inputPath);
+
         $dateIds = [];
         $dates = [];
         $dateCount = 0;
@@ -38,116 +37,71 @@ final class Parser
 
         $out = [];
 
-        $handle = fopen($inputPath, 'rb');
-        if ($handle === false) {
-            throw new Exception("Failed to open input file: {$inputPath}");
-        }
-        
-        stream_set_read_buffer($handle, 0);
+        $handle = \fopen($inputPath, 'rb');
+        \stream_set_read_buffer($handle, 0);
+        $remaining = $fileSize;
 
-        try {
-            $remaining = $fileSize;
-            
-            while ($remaining > 0) {
-                $toRead = $remaining > self::READ_CHUNK ? self::READ_CHUNK : $remaining;
-                $chunk = fread($handle, $toRead);
-                $chunkLen = strlen($chunk);
-                
-                if ($chunkLen === 0) break;
-                $remaining -= $chunkLen;
-                
-                $lastNl = strrpos($chunk, "\n");
-                if ($lastNl === false) {
-                    $this->processChunk($chunk, $dateIds, $out);
-                    continue;
-                }
-                
-                $this->processChunk(substr($chunk, 0, $lastNl + 1), $dateIds, $out);
-                
-                $tail = $chunkLen - $lastNl - 1;
-                if ($tail > 0) {
-                    fseek($handle, -$tail, SEEK_CUR);
-                    $remaining += $tail;
-                }
+        while ($remaining > 0) {
+            $toRead = $remaining > self::READ_CHUNK ? self::READ_CHUNK : $remaining;
+            $chunk = \fread($handle, $toRead);
+            $chunkLen = \strlen($chunk);
+
+            if ($chunkLen === 0) break;
+            $remaining -= $chunkLen;
+
+            $lastNl = \strrpos($chunk, "\n");
+            if ($lastNl === false) break;
+
+            $tail = $chunkLen - $lastNl - 1;
+            if ($tail > 0) {
+                \fseek($handle, -$tail, SEEK_CUR);
+                $remaining += $tail;
             }
-        } finally {
-            fclose($handle);
+
+            $p = self::PREFIX_LEN;
+            while ($p < $lastNl) {
+                $sep = \strpos($chunk, ',', $p);
+                if ($sep === false || $sep >= $lastNl) break;
+                $slug = \substr($chunk, $p, $sep - $p);
+                $date = \substr($chunk, $sep + 3, 8);
+                $dateKey = $dateIds[$date] ?? ('20' . $date);
+                if (!isset($out[$slug])) $out[$slug] = [];
+                $out[$slug][$dateKey] = ($out[$slug][$dateKey] ?? 0) + 1;
+                $p = $sep + 52;
+            }
         }
 
-        foreach ($out as $path => &$dateIdCounts) {
-            ksort($dateIdCounts);
+        \fclose($handle);
+
+        foreach ($out as &$dateIdCounts) {
+            \ksort($dateIdCounts);
         }
         unset($dateIdCounts);
 
         $this->jsonize($outputPath, $out, $dates);
     }
-    
-    private function processChunk(string $chunk, array $dateIds, array &$out): void
-    {
-        $pos = 0;
-        $chunkLen = strlen($chunk);
-        
-        while ($pos < $chunkLen) {
-            $nlPos = strpos($chunk, "\n", $pos);
-            if ($nlPos === false) {
-                $line = substr($chunk, $pos);
-                if ($line !== '') {
-                    $this->processLine($line, $dateIds, $out);
-                }
-                break;
-            }
-            
-            $line = substr($chunk, $pos, $nlPos - $pos);
-            if ($line !== '') {
-                $this->processLine($line, $dateIds, $out);
-            }
-            $pos = $nlPos + 1;
-        }
-    }
-    
-    private function processLine(string $line, array $dateIds, array &$out): void
-    {
-        // skipping https://stitcher.io/, it's all the same domain anyway
-        // also skipping 15ch of datetime
-        $line = substr($line, 19, -15);
-        
-        $commaPos = strpos($line, ',');
-        if ($commaPos === false) return;
-        
-        $path = substr($line, 0, $commaPos);
-        $dateFull = substr($line, $commaPos + 1, 10);
-
-        $date = substr($dateFull, 2);
-        
-        $dateKey = $dateIds[$date] ?? $dateFull;
-        
-        if (!isset($out[$path])) {
-            $out[$path] = [];
-        }
-        $out[$path][$dateKey] = ($out[$path][$dateKey] ?? 0) + 1;
-    }
 
     private function jsonize($filename, &$out, array $dates) {
-        $file = fopen($filename, 'wb');
-        stream_set_write_buffer($file, self::WRITE_BUF);
-        fwrite($file, '{');
+        $file = \fopen($filename, 'wb');
+        \stream_set_write_buffer($file, self::WRITE_BUF);
+        \fwrite($file, '{');
 
         $isFirst = true;
         foreach ($out as $k => $ds) {
-            $buf = $isFirst ? "\n    \"" : ",\n    \"";
-            $buf .= str_replace('/', '\\/', $k)."\": {\n";
+            $buf = $isFirst ? "\n    " : ",\n    ";
+            $isFirst = false;
+            $buf .= "\"\\/blog\\/" . \str_replace('/', '\\/', $k) . "\": {\n";
             $firstDate = true;
             foreach ($ds as $dateId => $v) {
-                $dateStr = is_int($dateId) ? '20' . $dates[$dateId] : $dateId;
+                $dateStr = \is_int($dateId) ? '20' . $dates[$dateId] : $dateId;
                 $buf .= ($firstDate ? '' : ",\n") . "        \"$dateStr\": $v";
                 $firstDate = false;
             }
             $buf .= "\n    }";
-            fwrite($file, $buf);
-            $isFirst = false;
+            \fwrite($file, $buf);
         }
 
-        fwrite($file, "\n}");
-        fclose($file);
+        \fwrite($file, "\n}");
+        \fclose($file);
     }
 }
